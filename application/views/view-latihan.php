2f20a7e45e5ec8ab85ca55a011107e03e8a21654
arifinilham <arifinilham392003@gmail.com>
2023-10-09 20:32:20 +0700

    first commit

---
+++ application/views/view-latihan.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+    <title>Latihan 1</title>
+</head>
+
+<body>
+    Hallo kawan.. Yuk kita beajar programing..!!!<br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    ini dari hasil permodelan denang methode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " + " . $hasil; ?>
+
+</body>
+
+</html> 
